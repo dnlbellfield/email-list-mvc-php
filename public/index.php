1ee0f656e1dba1c6fba5fba1e26a5f7cc013c040
Daniel Bellfield <dnlbellfield@gmail.com>
2023-05-01 10:06:40 -0700

    index.php

--- public/index.php
+++ public/index.php
@@ -6,10 +6,9 @@ require '../vendor/autoload.php';
 require '../core/helpers.php';
 require '../app/routes.php';
 if (file_exists(__DIR__ . '/../' . '.env')) {
-  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-  $dotenv->load();
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../' );
+  $dotenv->load(__DIR__);
 }
-
 
 // require '../app/views/index.view.php';
 
