8f27c69c9e57917d8fb785b255e1a223838abf30
Daniel Bellfield <dnlbellfield@gmail.com>
2023-05-01 12:15:33 -0700

    index.php

--- core/DBConnection.php
+++ core/DBConnection.php
@@ -6,7 +6,7 @@ use PDO;
 class DBConnection{
   public static function start() {
     try {
-      return $pdo = new PDO("{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};PORT={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}",$_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+      return $pdo = new PDO("{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']}",$_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
     } catch(PDOException $e) {
       dd($e->getMessage());
     } 
