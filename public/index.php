69e61891e6dc195a482c4cd8b4dbe78027341cae
Daniel Bellfield <dnlbellfield@gmail.com>
2023-05-02 08:54:45 -0700

    index.view.php

--- public/index.php
+++ public/index.php
@@ -5,15 +5,11 @@ use Core\DBConnection;
 require '../vendor/autoload.php';
 require '../core/helpers.php';
 require '../app/routes.php';
+
 if (file_exists(__DIR__ . '/../' . '.env')) {
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../' );
   $dotenv->load(__DIR__);
 }
-
-// require '../app/views/index.view.php';
-
-
-// $EmailModel = new EmailModel(DBConnection::start());
 
  // Run  bramus router
 $router->run();
